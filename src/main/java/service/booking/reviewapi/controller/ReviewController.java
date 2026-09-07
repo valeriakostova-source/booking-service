991fd00464b3449bcc7c2912d432eb50df8ca627
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-09-07 04:28:09 +0200

    Refactor: -Changed variable name to better indicate that it doesnt take an id, but a room number

--- src/main/java/service/booking/reviewapi/controller/ReviewController.java
+++ src/main/java/service/booking/reviewapi/controller/ReviewController.java
@@ -33,8 +33,8 @@ public class ReviewController {
     }
 
     @GetMapping("/reviews/{id}")
-    public ReviewResponseDto getReviewById(@RequestHeader("Authorization") String token, @PathVariable Long id) {
-        return reviewClient.getReviewById(token, id);
+    public ReviewResponseDto getReviewById(@RequestHeader("Authorization") String token, @PathVariable("id") Long roomNumber) {
+        return reviewClient.getReviewById(token, roomNumber);
     }
 
     @GetMapping("/reviews/room/avgRating/{id}")
