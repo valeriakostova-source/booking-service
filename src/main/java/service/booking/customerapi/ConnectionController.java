9abb90212f99ce20d31e53e491a15d0c487060e4
soppitopp <soppitopp2000@hotmail.com>
2026-09-07 14:59:28 +0200

    fix: IntelliJ bug wrongly read mark retrieve + add deleteAccount().

--- src/main/java/service/booking/customerapi/ConnectionController.java
+++ src/main/java/service/booking/customerapi/ConnectionController.java
@@ -2,6 +2,8 @@ package service.booking.customerapi;
 
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.http.StreamingHttpOutputMessage.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +15,7 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
 
 import service.booking.dto.CreateCustomerRequest;
+import service.booking.dto.CustomerInfo;
 import service.booking.dto.LoginDto;
 import service.booking.dto.UpdateDto;
 
@@ -68,12 +71,14 @@ public class ConnectionController {
     @GetMapping("/info")
     public ResponseEntity<?> myPageData(@RequestHeader("Authorization") String jwt) {
         try {
-            return restClient
+            RestClient.RequestHeadersSpec<?> request = (RestClient.RequestHeadersSpec<?>) restClient
                     .get()
                     .uri("/api/customers/info")
-                    .header("Authorization", jwt)
+                    .header("Authorization", jwt);
+            return (request
                     .retrieve()
-                    .toEntity(service.booking.dto.CustomerInfo.class);
+                    .toEntity(CustomerInfo.class)
+            );
         } catch (HttpClientErrorException e) {
             return (ResponseEntity
                     .status(e.getStatusCode())
@@ -86,15 +91,34 @@ public class ConnectionController {
     public ResponseEntity<?> updateCustomerInfo(@RequestHeader("Authorization") String jwt,
                                                 @RequestBody UpdateDto update) {
         try {
-            return restClient
+            return (restClient
                     .post()
                     .uri("/api/customers/update")
                     .header("Authorization", jwt)
                     .body(update)
                     .retrieve()
-                    .toEntity(Object.class);
+                    .toEntity(Object.class)
+            );
         } catch (HttpClientErrorException e) {
-            System.err.println("catch update");
+            return (ResponseEntity
+                    .status(e.getStatusCode())
+                    .body(e.getResponseBodyAsString())
+            );
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteAccount(@RequestHeader("Authorization") String jwt) {
+        try {
+            RestClient.RequestHeadersSpec<?> request = (RestClient.RequestHeadersSpec<?>) restClient
+                    .delete()
+                    .uri("/api/customers/delete")
+                    .header("Authorization", jwt);
+            return (request
+                    .retrieve()
+                    .toEntity(Object.class)
+            );
+        } catch (HttpClientErrorException e) {
             return (ResponseEntity
                     .status(e.getStatusCode())
                     .body(e.getResponseBodyAsString())
