b541ecb54af826e8007558450c28de9c9c1ad591
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-09-10 03:15:30 +0200

    Changed variable to better represent what is being used.

--- src/main/java/service/booking/reviewapi/dto/ReviewResponseDto.java
+++ src/main/java/service/booking/reviewapi/dto/ReviewResponseDto.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 public record ReviewResponseDto(
         Long id,
         Long userId,
-        Long roomId,
+        Integer roomNumber,
         String reviewContent,
         Integer reviewScore,
         LocalDateTime creationDate,
